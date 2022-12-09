608b8cb028ebf4d5880cf107ef001b444c0e03f9
jk <jk@MacBook-Air-jk.local>
2022-12-09 07:32:15 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Category, Food, HeadlineCards, Hero, Navbar } from './components'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  )
+}
+
+export default App
